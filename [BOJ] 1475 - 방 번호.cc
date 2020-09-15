f6b71feddaafcbf8ce850cdddd7065e882fba657
dong-min-god <55154178+dong-min-god@users.noreply.github.com>
2020-09-15 11:37:53 +0900

    [BOJ]

---
+++ [BOJ] 1475 - 방 번호.cc
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	int sum = 0;
+	int tmp;
+	int count[10] = { 0, };
+	int max;
+	scanf("%d", &N);
+	if (N == 0)
+		printf("1");
+	else {
+		while (N > 0) {
+			tmp = N % 10;
+			count[tmp]++;
+			N /= 10;
+		}
+
+		tmp = ((count[6] + count[9]) + 1) / 2;
+		count[6] = tmp;
+		count[9] = tmp;
+		max = count[0];
+		for (int i = 1; i < 10; i++) {
+			if (count[i] >= max)
+				max = count[i];
+			else
+				continue;
+		}
+		printf("%d", max);
+	}
+}
